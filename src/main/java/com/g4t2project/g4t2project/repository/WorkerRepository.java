1f5c53c062828825d76e28e653f1ff68c43a3b60
iamAK <akannappan27@gmail.com>
2024-10-23 20:50:50 +0800

    working on create cleaningTask endpoint

--- src/main/java/com/g4t2project/g4t2project/repository/WorkerRepository.java
+++ src/main/java/com/g4t2project/g4t2project/repository/WorkerRepository.java
@@ -6,7 +6,7 @@ import com.g4t2project.g4t2project.entity.*;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import java.time.LocalDate;
-import com.g4t2project.g4t2project.entity.CleaningTask;
+import java.util.List;
 
 public interface WorkerRepository extends JpaRepository<Worker, Long> {
     Optional<Worker> findFirstByAvailableTrue();
@@ -15,4 +15,12 @@ public interface WorkerRepository extends JpaRepository<Worker, Long> {
      @Query("SELECT w FROM Worker w WHERE w.available = true AND w.deployed = false AND NOT EXISTS (" +
      "SELECT t FROM CleaningTask t WHERE t.worker = w AND t.date = :date AND t.shift = :shift)")
     Optional<Worker> findAvailableWorker(@Param("date") LocalDate date, @Param("shift") CleaningTask.Shift shift);
+
+    // Custom query to find all deployed workers
+    @Query("SELECT w FROM Worker w WHERE w.curPropertyId > :value")
+    List<Worker> findAllDeployed(@Param("value") int value);
+
+    // Custom query to find all not deployed workers
+    @Query("SELECT w FROM Worker w WHERE w.curPropertyId = :value")
+    List<Worker> findAllNotDeployed(@Param("value") int value);
 }
