193a4ea937eb3000a16a155022f903c82c0371d5
iamAK <akannappan27@gmail.com>
2024-10-26 09:54:20 +0800

    edits to cleaningTaskRepo

--- src/main/java/com/g4t2project/g4t2project/repository/CleaningTaskRepository.java
+++ src/main/java/com/g4t2project/g4t2project/repository/CleaningTaskRepository.java
@@ -1,6 +1,8 @@
 package com.g4t2project.g4t2project.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.g4t2project.g4t2project.entity.CleaningTask;
 import java.util.List;
@@ -9,5 +11,7 @@ import java.time.LocalDate;
 
 @Repository
 public interface CleaningTaskRepository extends JpaRepository<CleaningTask, Integer> {
-    List<CleaningTask> findTasksByWorkerAndDate(Worker worker, LocalDate date);
+    @Query("SELECT c FROM CleaningTask c WHERE c.worker = :worker AND c.date = :date")
+    List<CleaningTask> findTasksByWorkerAndDate(@Param("worker") Worker worker, @Param("date") LocalDate date);
+
 }
