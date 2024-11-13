88b88c6b36ee5da0ce79025427f23b1da769cf91
ChyeShaoXuan <142379429+ChyeShaoXuan@users.noreply.github.com>
2024-11-13 22:54:30 +0800

    removed loading

--- front-end/app/admin/JobStatisticsDashboard/page.tsx
+++ front-end/app/admin/JobStatisticsDashboard/page.tsx
@@ -6,19 +6,9 @@ import Loading from "@/components/ui/loading";
 import JobStatisticsDashboard from '@/components/admin/JobStatisticsDashboard/JobStatisticsDashboard'; 
 
 function JobStatisticsPage() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-
-    return () => clearTimeout(timer);
-  }, []);
 
   return (
     <div>
-      {loading && <Loading />}
       <Navbar />
       <JobStatisticsDashboard />
     </div>
--- front-end/app/admin/ViewClients/page.tsx
+++ front-end/app/admin/ViewClients/page.tsx
@@ -6,20 +6,9 @@ import Navbar from "@/components/ui/adminpagesnavbar"
 import Loading from "@/components/ui/loading"
 
 export default function ViewClientsPage() {
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    // Simulate a loading delay
-    const timer = setTimeout(() => {
-      setLoading(false)
-    }, 2000)
-
-    return () => clearTimeout(timer)
-  }, [])
 
   return (
     <div>
-      {loading && <Loading />}
       <Navbar />
       <ViewClients />
     </div>
