7ff5c54bdf558df37bf6712b6f4092d18c596c31
we14un <snakeylim@gmail.com>
2024-11-13 20:40:41 +0800

    remove checking of user authentication for now cuz caused errors

--- front-end/app/context/useAuth.jsx
+++ front-end/app/context/useAuth.jsx
@@ -10,31 +10,31 @@ export const useAuth = () => {
 };
 
 export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
+    // const [user, setUser] = useState(null);
 
-    useEffect(() => {
-        // Fetch user data from the backend or local storage
-        const fetchUser = async () => {
-            const token = localStorage.getItem('jwtToken');
-            if (!token) {
-                console.error('No JWT token found');
-                return;
-            }
+    // useEffect(() => {
+    //     // Fetch user data from the backend or local storage
+    //     const fetchUser = async () => {
+    //         const token = localStorage.getItem('jwtToken');
+    //         if (!token) {
+    //             console.error('No JWT token found');
+    //             return;
+    //         }
 
-            try {
-                const res = await axios.get('http://localhost:8080/authentication/user', {
-                    headers: {
-                        Authorization: `Bearer ${token}`
-                    }
-                });
-                setUser(res.data);
-            } catch (error) {
-                console.error('Failed to fetch user', error);
-            }
-        };
+    //         try {
+    //             const res = await axios.get('http://localhost:8080/authentication/user', {
+    //                 headers: {
+    //                     Authorization: `Bearer ${token}`
+    //                 }
+    //             });
+    //             setUser(res.data);
+    //         } catch (error) {
+    //             console.error('Failed to fetch user', error);
+    //         }
+    //     };
 
-        fetchUser();
-    }, []);
+    //     fetchUser();
+    // }, []);
 
     const logout = () => {
         // Clear the JWT token from local storage
@@ -43,7 +43,7 @@ export const AuthProvider = ({ children }) => {
     };
 
     return (
-        <AuthContext.Provider value={{ user, setUser, logout }}>
+        <AuthContext.Provider value={{logout }}>
             {children}
         </AuthContext.Provider>
     );
